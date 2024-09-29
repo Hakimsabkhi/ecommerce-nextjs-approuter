92f798c412bcf957319112004235ce1afbff448c
Hakimsabkhi <62035010+Hakimsabkhi@users.noreply.github.com>
2024-09-29 11:07:52 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -9,17 +9,6 @@ const nextConfig = {
           },
         ],
       },
-      webpack: (config, { isServer }) => {
-        if (isServer) {
-          // Ensure externals is defined and is an array
-          if (!config.externals) {
-            config.externals = [];
-          }
-          config.externals.push('nodemailer');
-        }
-        return config;
-      },
-      
 };
 
 export default nextConfig;
