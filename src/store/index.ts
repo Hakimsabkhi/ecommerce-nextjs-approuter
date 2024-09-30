a2b2a676056f66064311cdf7c9328f463b007bae
bayoudh <mohamed.salah.bayoudh@gmail.com>
2024-09-30 12:29:57 +0100

    fix storage carte

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,24 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+//import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import cartReducer from './cartSlice';
 
+const createNoopStorage = () => {
+  return {
+    getItem(_key: string) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: string, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: string) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
 // Combine your reducers
 const rootReducer = combineReducers({
   cart: cartReducer,
@@ -12,6 +28,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root', 
   storage,
+ 
 };
 
 // Create a persisted reducer
