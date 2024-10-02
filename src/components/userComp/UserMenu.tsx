98e03b5b5e0d16b4c976dc2425a68dd67d13538e
hakim <hakimsabkhi001@gmail.com>
2024-10-02 12:08:55 +0200

    fix auth

--- src/components/userComp/UserMenu.tsx
+++ src/components/userComp/UserMenu.tsx
@@ -4,29 +4,16 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { FaRegUserCircle } from 'react-icons/fa';
 import Dropdown from "@/components/Dropdown";
-import { useSession } from "next-auth/react";
+import { useSessionData } from '@/content/session/useSessionData';
 
 const UserMenu: React.FC = () => {
-  const { data: session } = useSession(); // Get session data and status
+  const { session, loading } = useSessionData();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
- /*  const [isLoading, setIsLoading] = useState(true); // State to track loading
- */
+
   const toggleDropdown = () => {
     setIsDropdownOpen((prev) => !prev);
   };
 
-/*   useEffect(() => {
-    if (status === "loading") {
-      setIsLoading(true); // Set loading to true while fetching
-    } else {
-      setIsLoading(false); // Set loading to false after fetching
-    }
-  }, [status]); */
-
-/*   // Display a loading state or the user menu based on session
-  if (isLoading) {
-    return <></>; // Optional loading indicator
-  } */
 
   if (session?.user) {
     return (
