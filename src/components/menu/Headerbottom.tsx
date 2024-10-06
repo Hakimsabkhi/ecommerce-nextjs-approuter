f8d405038ab68c054a63e60f31e2ac6f9f85b97e
unknown <hakimsabkhi001@gmail.com>
2024-10-06 10:01:40 +0200

    fix loading data

--- src/components/menu/Headerbottom.tsx
+++ src/components/menu/Headerbottom.tsx
@@ -2,50 +2,44 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// Type definition for category data
+// Type definition for category data (if you're using TypeScript)
 interface Category {
   name: string;
   logoUrl?: string;
 }
 
-// Function to fetch categories from the API
-const fetchCategories = async (): Promise<Category[]> => {
-  try {
-    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/category/getAllCategory`, {
-      method: 'GET',
-     
-      next: { revalidate: 0 }, // Disable caching to always fetch the latest data
-    })
-    if (!res.ok) {
-      throw new Error('Failed to fetch categories');
-    }
-    const data: Category[] = await res.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return [];
+async function fetchcategoryData() {  
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/category/getAllCategory`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+      throw new Error('Failed to fetch data');
   }
-};
-const Headerbottom = async () => {
-  // Fetch categories in a server component
-  const categories = await fetchCategories();
+  return res.json();
+}
 
+
+const Headerbottom: React.FC = async () => {
+ const categories = await  fetchcategoryData()
   if (categories.length === 0) {
-    return null; // No categories found
+    return <></>;
   }
 
   return (
     <header>
       <nav className="w-full h-[72px] flex justify-center bg-white max-lg:hidden">
-        <div className="flex justify-between w-[90%] max-xl:w-[95%] font-bold items-center text-xl max-2xl:text-sm">
-          {categories.map((category: Category) => (
+        <div className="flex justify-start gap-8 w-fit max-xl:w-[95%] font-bold items-center text-xl max-2xl:text-sm">
+          {categories?.map((category: Category) => (
             <Link 
               href={`/${category.name}`} 
               key={category.name} 
               className="flex items-center gap-3 duration-300 hover:text-orange-400" 
               aria-label={category.name}
             >
-              {category.logoUrl && (
+              {category?.logoUrl && (
                 <Image
                   src={category.logoUrl}
                   alt={category.name}
