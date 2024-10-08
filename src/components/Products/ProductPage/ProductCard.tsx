91ae68ff7908e40d7ce87cb08a087f65ccca58bd
unknown <hakimsabkhi001@gmail.com>
2024-10-08 17:55:51 +0200

    update card and api

--- src/components/Products/ProductPage/ProductCard.tsx
+++ src/components/Products/ProductPage/ProductCard.tsx
@@ -3,10 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaEye, FaRegHeart } from "react-icons/fa";
-import { FaCartShopping, FaHeart } from "react-icons/fa6";
-import { star } from "@/assets/image";
-
+import { FaEye, FaRegHeart, FaHeart, FaCartShopping, FaStar } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem } from "@/store/cartSlice";
 import { RootState } from "@/store";
@@ -31,11 +28,11 @@ interface ProductData {
   material?: string;
   status?: string;
   category: Category;
-  slug:string;
+  slug: string;
 }
 interface Category {
   name: string;
-  slug:string;
+  slug: string;
 }
 
 interface ProductCardProps {
@@ -43,43 +40,30 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
- /*  const params = useParams() as { product?: string };
-  const product = params.product;
- */
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
     setClicked(!clicked);
   };
 
-  const noimage =
-    "https://res.cloudinary.com/dx499gc6x/image/upload/v1723623372/na_mma1mw.webp";
-
-  const items = useSelector((state: RootState) => state.cart.items);
- 
-
   const dispatch = useDispatch();
   const addToCartHandler = (product: ProductData, quantity: number) => {
-    // Ensure dispatch is available
- 
-   // Dispatch the action with item and quantity
-   dispatch(addItem({ item: product, quantity }));
- 
-   // Show success notification
-   toast.success(`${product.name} added to cart !`);
- };
+    dispatch(addItem({ item: product, quantity }));
+    toast.success(`${product.name} added to cart!`);
+  };
+
   return (
-    <div className="bg-white rounded-lg duration-500 lg:group-hover:scale-[0.85] lg:hover:!scale-100 h-[481px] max-md:h-[320px] relative m-4">
+    <div className=" flex gap-2 h-fit flex-col duration-500 lg:group-hover:scale-[0.85] lg:hover:!scale-100 max-md:h-[320px] relative">
       <Link href={`/${item.category.slug}/${item.slug}`}>
         <Image
-          className="absolute inset-0 mx-auto top-5"
-          src={item.imageUrl || noimage}
+          className=" w-full h-auto mx-auto top-5"
+          src={item.imageUrl || ""}
           alt={item.name}
           height={300}
           width={300}
         />
       </Link>
-      <div className="flex-col flex bottom-0 gap-2 absolute w-full px-2">
+      <div className="flex-col flex bottom-0 gap-2 w-full">
         <Link href={`/${item.category.slug}/${item.slug}`}>
           <div className="h-24 max-md:h-20">
             <p className="text-gray-700 cursor-pointer text-3xl max-md:text-xl font-bold">
@@ -92,7 +76,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
                     {item.price - item.price * (item.discount / 100)} TND
                   </p>
                   <span className="text-primary line-through text-xl font-bold">
-                  <p className="text-gray-300" >{item.price} TND</p>  
+                    <p className="text-gray-300">{item.price} TND</p>
                   </span>
                 </div>
               ) : (
@@ -104,47 +88,51 @@ const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
           </div>
         </Link>
         <div className="flex gap-2 items-center">
-          <Image className="size-5 max-md:size-4" src={star} alt="star" />
-          <Image className="size-5 max-md:size-4" src={star} alt="star" />
-          <Image className="size-5 max-md:size-4" src={star} alt="star" />
-          <Image className="size-5 max-md:size-4" src={star} alt="star" />
-          <Image className="size-5 max-md:size-4" src={star} alt="star" />
+          {[...Array(5)].map((_, index) => (
+            <FaStar key={index} className="text-yellow-500 size-5 max-md:size-4" />
+          ))}
           <p className="flex gap-2 text-xl max-md:text-sm font-bold items-center">
             5
           </p>
         </div>
         <div className="flex mb-1 text-lg max-md:text-sm justify-between">
-
-        {item.status != 'out-of-stock' ? (item.stock > 0 ? ( <button
-            onClick={() => {
-              addToCartHandler(item,1);
-            }}
-            className="AddtoCart bg-primary hover:bg-[#15335D] text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box"
-          >
-            <p className="absolute  flex items-center justify-center w-full h-full transition-all duration-300 transform lg:group-hover/box:translate-x-[10%] ease">
-              Add to cart
-            </p>
-            <p className="text-white absolute flex items-center justify-center w-full h-full duration-300 -translate-x-[100%] lg:group-hover/box:translate-x-[-30%] ease">
-              <FaCartShopping
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-              />
-            </p>
-          </button> ) : (
-        <button className="AddtoCart bg-gray-400 hover:bg-gray-500 text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box" disabled>
-        <p className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform lg:group-hover/box:translate-x-[10%] ease">
-        Out of stock
-        </p>
-        
-      </button>
-     
-      )):(<button className="AddtoCart bg-gray-400 hover:bg-gray-500 text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box" disabled>
-        <p className="absolute flex items-center justify-center w-full h-full transition-all duration-300   ease">
-        Out of stock
-        </p>
-       
-      </button>)}
+          {item.status !== "out-of-stock" ? (
+            item.stock > 0 ? (
+              <button
+                onClick={() => addToCartHandler(item, 1)}
+                className="AddtoCart bg-primary hover:bg-[#15335D] text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box"
+              >
+                <p className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform lg:group-hover/box:translate-x-[10%] ease">
+                  Add to cart
+                </p>
+                <p className="text-white absolute flex items-center justify-center w-full h-full duration-300 -translate-x-[100%] lg:group-hover/box:translate-x-[-30%] ease">
+                  <FaCartShopping
+                    className="w-6 h-6"
+                    aria-hidden="true"
+                    fill="currentColor"
+                  />
+                </p>
+              </button>
+            ) : (
+              <button
+                className="AddtoCart bg-gray-400 hover:bg-gray-500 text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box"
+                disabled
+              >
+                <p className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform lg:group-hover/box:translate-x-[10%] ease">
+                  Out of stock
+                </p>
+              </button>
+            )
+          ) : (
+            <button
+              className="AddtoCart bg-gray-400 hover:bg-gray-500 text-white w-[50%] max-md:rounded-[3px] max-2xl:text-sm group/box"
+              disabled
+            >
+              <p className="absolute flex items-center justify-center w-full h-full transition-all duration-300 ease">
+                Out of stock
+              </p>
+            </button>
+          )}
 
           <a href={`/${item.category.slug}/${item.slug}`} className="w-[30%]">
             <button className="AddtoCart bg-white max-md:rounded-[3px] w-full group/box text-primary border border-primary">
@@ -173,7 +161,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ item }) => {
               />
             </p>
             <p
-              className={`absolute flex items-center justify-center w-full h-full  group-hover/box:opacity-100`}
+              className={`absolute flex items-center justify-center w-full h-full group-hover/box:opacity-100`}
             >
               <FaHeart
                 className="w-5 h-5 max-2xl:w-3 max-2xl:h-3"
