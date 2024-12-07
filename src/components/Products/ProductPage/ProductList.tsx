13b538b66fd6f18bbd30141f8b02ff2b06ffee6a
Mohamed Salah Bayoudh <mohamed.salah.bayoudh@gmail.com>
2024-12-07 11:53:09 +0100

    product fatch

--- src/components/Products/ProductPage/ProductList.tsx
+++ src/components/Products/ProductPage/ProductList.tsx
@@ -39,15 +39,15 @@ const ProductList: React.FC<ProductListProps> = ({ products   }) => {
   return (
     <div>
     
-      {products.length !== 0  ?(
+    
         <div className="grid group grid-cols-3 max-md:grid-cols-1 max-lg:grid-cols-2 max-md:gap-3 gap-8">
           {products.map((item) => (
             <ProductCard key={item._id} item={item} />
           ))}
         </div>
-      ):(
-        <div className=" text-xl  w-full h-full flex justify-center ">no products available</div>
-      )}
+     
+       
+     
     </div>
   );
 };
